7b20da8a9891efef0f575ab4bfeefa56643d12fb
rahul-1729 <rk9763981@gmail.com>
2025-11-29 23:21:19 +0530

    stage 2

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           THis is to test staging
+           THis is to test staging 2
         </p>
         <a
           className="App-link"
